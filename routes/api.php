76ebfb2b22e0013bf79ac7816224433fa150d60d
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-09-18 22:53:06 -0300

    feat: add info route

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\PessoaController;
 use App\Models\Pessoa;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/katatau', function () {
-    return response()->json(['message' => 'Katatau was here!']);
+Route::get('/info', function () {
+    phpinfo();
 });
 
 Route::post('/login', [PessoaController::class, 'login']);
